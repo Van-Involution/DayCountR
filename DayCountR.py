ff326184482bec6dab7dae4d80f88e8cd1990a12
Van_nya <van114514@outlook.com>
2021-01-26 01:07:12 +0800

    Reforged from plg "daycount" of Fallen_Breath

--- DayCountR.py
+++ DayCountR.py
@@ -1,16 +1,63 @@
 # -*- coding: UTF-8 -*-
 
+from yaml import load, FullLoader
+from datetime import date
+from pathlib import Path
+
+from mcdreforged.api.types import ServerInterface, CommandSource
+from mcdreforged.api.command import Literal
+
 PLUGIN_METADATA = {
     'id': 'day_count_reforged',
     'version': '1.0.0',
     'name': 'DayCountR',
-    'description': '',
+    'description': 'Use command "!!days" to get day count since server set up',
     'author': [
         'Van_Involution',  # Reforged to fit MCDR 1.x
-        'TISUnion'  # Source of inspiration
+        'Fallen_Breath'  # Source of inspiration
     ],
-    'link': 'https://github.com/Van-Involution/MCDR-Plugins',
+    'link': 'https://github.com/Van-Involution/DayCountR',
     'dependencies': {
         'mcdreforged': '>=1.0.0',
     }
 }
+
+NAME = PLUGIN_METADATA['name']
+DEFAULT_CONFIG_PATH = f'config/{NAME}.yml'
+DEFAULT_CONFIG = '''# Configure file for DayCountR
+
+# Date when server set up like "YYYY-MM-DD"
+# 开服日期，形如 “YYYY-MM-DD”
+start_date: {today}
+
+# Customize your reply msg to cmd-src, use {{days}} as format key
+# 自定义回复消息，用 {{days}} 作为格式化键名
+reply_msg: Today, server has been set up for \u00a7e{{days}}\u00a7r days!
+'''.format(today=date.today().strftime('%Y-%m-%d'))
+
+
+def get_config(server: ServerInterface):
+    if not Path(DEFAULT_CONFIG_PATH).is_file():
+        server.logger.info('Fail to read config file, using default value')
+        with open(file=DEFAULT_CONFIG_PATH, mode='w') as cfg_0:
+            cfg_0.write(DEFAULT_CONFIG)
+    with open(file=DEFAULT_CONFIG_PATH, mode='r') as cfg:
+        return load(stream=cfg, Loader=FullLoader)
+
+
+def get_day_count(start_date: date):
+    today_date = date.today()
+    return (today_date - start_date).days
+
+
+def reply_msg(src: CommandSource):
+    config = get_config(src.get_server())
+    day_count = get_day_count(config['start_date'])
+    src.reply(config['reply_msg'].format(days=day_count))
+
+
+def on_load(server: ServerInterface, prev):
+    server.register_help_message(prefix='!!days', message='Get day count since server set up')
+    server.register_command(
+        Literal('!!days').runs(reply_msg)
+    )
