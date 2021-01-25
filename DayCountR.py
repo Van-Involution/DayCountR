c2805b4927e4565985e5e9ec5004c90a9ae3afbc
Van_nya <van114514@outlook.com>
2021-01-25 21:35:00 +0800

    rua

---
+++ DayCountR.py
@@ -0,0 +1,16 @@
+# -*- coding: UTF-8 -*-
+
+PLUGIN_METADATA = {
+    'id': 'day_count_reforged',
+    'version': '1.0.0',
+    'name': 'DaycountR',
+    'description': '',
+    'author': [
+        'Van_Involution',  # Reforged to fit MCDR 1.x
+        'TISUnion'  # Source of inspiration
+    ],
+    'link': 'https://github.com/Van-Involution/MCDR-Plugins',
+    'dependencies': {
+        'mcdreforged': '>=1.0.0',
+    }
+}
