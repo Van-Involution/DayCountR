6c8162d0b67743426ccae367c8ea8d9df926bd75
Van_nya <van114514@outlook.com>
2021-01-30 00:29:56 +0800

    Add error control

--- DayCountR.py
+++ DayCountR.py
@@ -1,11 +1,11 @@
 # -*- coding: UTF-8 -*-
 
-from yaml import load, FullLoader
 from datetime import date
 from pathlib import Path
 
 from mcdreforged.api.types import ServerInterface
 from mcdreforged.api.command import Literal
+from mcdreforged.api.rtext import RText
 
 PLUGIN_METADATA = {
     'id': 'day_count_reforged',
@@ -38,23 +38,33 @@ reply_msg: Today, server has been set up for §e{{days}}§r days!
 
 
 def get_config(server: ServerInterface):
-    if not Path(DEFAULT_CONFIG_PATH).is_file():
-        server.logger.info('Fail to read config file, using default value')
-        with open(file=DEFAULT_CONFIG_PATH, mode='w') as cfg_0:
-            cfg_0.write(DEFAULT_CONFIG)
-    with open(file=DEFAULT_CONFIG_PATH, mode='r') as cfg:
-        return load(stream=cfg, Loader=FullLoader)
+    try:
+        from yaml import load, FullLoader
+    except Exception:
+        server.logger.warning('Failed to import package PyYAML (https://pypi.org/project/PyYAML), install it by command "pip install PyYAML"')
+        return None
+    else:
+        if not Path(DEFAULT_CONFIG_PATH).is_file():
+            server.logger.info('Fail to read config file, using default value')
+            with open(DEFAULT_CONFIG_PATH, 'w') as default_cfg:
+                default_cfg.write(DEFAULT_CONFIG)
+        with open(DEFAULT_CONFIG_PATH, 'r') as cfg:
+            return load(cfg, FullLoader)
 
 
 def get_day_count(server: ServerInterface):  # You can call this function in other modules
-    config = get_config(server)
-    today_date = date.today()
-    start_date = config.get('start_date', today_date)
-    return config.get('reply_msg', 'Today, server has been set up for §e{days}§r days!').format(days=(today_date - start_date).days)
+    if (config := get_config(server)) is not None:
+        start_date = config.get('start_date', (today_date := date.today()))
+        return RText(
+            config.get('reply_msg', 'Today, server has been set up for §e{days}§r days!')
+            .format(days=(today_date - start_date).days)
+        ).h(f'§l{start_date}§r -> §l{today_date}§r')
+    else:
+        return f'§cSomething of plugin {NAME} is going wrong, please let admins look up the console!§r'
 
 
 def on_load(server: ServerInterface, prev):
-    server.register_help_message(prefix='!!days', message='Get day count since server set up')
+    server.register_help_message('!!days', 'Get day count since server set up')
     server.register_command(
         Literal('!!days').runs(lambda src: src.reply(get_day_count(server)))
     )
