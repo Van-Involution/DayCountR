3453a54401017adde5bc5e5c341bbb3a762648c8
Van_nya <van114514@outlook.com>
2021-02-11 16:10:18 +0800

    Use ruamel.yaml as MCDR requires

--- DayCountR.py
+++ DayCountR.py
@@ -3,6 +3,8 @@
 from datetime import date
 from pathlib import Path
 
+from ruamel.yaml import load, Loader
+
 from mcdreforged.api.types import ServerInterface
 from mcdreforged.api.command import Literal
 from mcdreforged.api.rtext import RText
@@ -36,31 +38,22 @@ reply_msg: Today, server has been set up for §e{{days}}§r days!
 
 
 def get_config(server: ServerInterface):
-    try:
-        from yaml import load, FullLoader
-    except Exception:
-        server.logger.warning('Failed to import package PyYAML (https://pypi.org/project/PyYAML), install it by command "pip install PyYAML"')
-        return None
-    else:
-        if not Path(DEFAULT_CONFIG_PATH).is_file():
-            server.logger.info('Fail to read config file, using default value')
-            with open(DEFAULT_CONFIG_PATH, 'w') as default_cfg:
-                default_cfg.write(DEFAULT_CONFIG)
-        with open(DEFAULT_CONFIG_PATH, 'r') as cfg:
-            return load(cfg, FullLoader)
+    if not Path(DEFAULT_CONFIG_PATH).is_file():
+        server.logger.info('Fail to read config file, using default value')
+        with open(DEFAULT_CONFIG_PATH, 'w') as default_cfg:
+            default_cfg.write(DEFAULT_CONFIG)
+    with open(DEFAULT_CONFIG_PATH, 'r') as cfg:
+        return load(cfg, Loader)
 
 
 def get_day_count(server: ServerInterface):
     config = get_config(server)
-    if config is not None:
-        today_date = date.today()
-        start_date = config.get('start_date', today_date)
-        return RText(
-            config.get('reply_msg', 'Today, server has been set up for §e{days}§r days!')
-            .format(days=(today_date - start_date).days)
-        ).h(f'§l{start_date}§r -> §l{today_date}§r')
-    else:
-        return f'§cSomething of plugin {NAME} is going wrong, please let admins look up the console!§r'
+    today_date = date.today()
+    start_date = config.get('start_date', today_date)
+    return RText(
+        config.get('reply_msg', 'Today, server has been set up for §e{days}§r days!')
+        .format(days=(today_date - start_date).days)
+    ).h(f'§l{start_date}§r -> §l{today_date}§r')
 
 
 def on_load(server: ServerInterface, prev):
