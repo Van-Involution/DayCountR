ae138a10d083b4be5cfadf0612c2db189931f77a
Van_Nya <admin@ruavan.moe>
2021-06-06 14:13:26 +0800

    Remove shit-like config file

--- DayCountR.py
+++ DayCountR.py
@@ -1,7 +1,4 @@
-from datetime import date
-from pathlib import Path
-
-from ruamel.yaml import load, Loader
+from datetime import datetime
 
 from mcdreforged.api.types import ServerInterface
 from mcdreforged.api.command import Literal
@@ -18,61 +15,48 @@ PLUGIN_METADATA = {
     ],
     'link': 'https://github.com/Van-Nya/DayCountR',
     'dependencies': {
-        'mcdreforged': '>=1.0.0',
+        'mcdreforged': '>=1.0.0'
     }
 }
 
-NAME = PLUGIN_METADATA['name']
-DEFAULT_CONFIG_PATH = f'config/{NAME}.yml'
-DEFAULT_CONFIG = '''# Configure file for DayCountR
-# Check https://github.com/Van-Involution/DayCountR for detail
 
-# Date of server set up like "YYYY-MM-DD"
-start_date: {today}
-
-# Customize reply message, use {{days}} as format key
-reply_msg: Today, server has been set up for §e{{days}}§r days!
-'''.format(today=date.today().strftime('%Y-%m-%d'))
-
-config: dict
-today_date: date
-start_date: date
+class Config:
+    """
+    Edit config here
+    """
+    START_DATE = '2019-09-07'  # format: YYYY-MM-DD
+    REPLY_MSG = '今天已经开服 §e{}§r 天了!' # use {} as the key
 
 
-def get_config(server: ServerInterface):
-    if not Path(DEFAULT_CONFIG_PATH).is_file():
-        server.logger.info('Fail to read config file, using default value')
-        with open(DEFAULT_CONFIG_PATH, 'w') as default_cfg:
-            default_cfg.write(DEFAULT_CONFIG)
-    with open(DEFAULT_CONFIG_PATH, 'r') as cfg:
-        return load(cfg, Loader)
+class GetDayCount:
+    """
+    DO NOT edit here
+    """
+    def __init__(self):
+        self.today_date = datetime.today()
+        self.start_date = datetime.strptime(Config.START_DATE, '%Y-%m-%d')
+        self.reply_msg = Config.REPLY_MSG
+
+    @property
+    def days(self) -> str:
+        return str((self.today_date - self.start_date).days)
+
+    def __call__(self) -> RText:
+        return RText(self.reply_msg.format(self.days))\
+            .h(f'§l{self.start_date}§r -> §l{self.today_date}§r')
 
 
-def getday():
+def getday() -> str:
     """
     An API for https://github.com/eagle3236/joinMOTD_Plus
 
     :return: A string shows days from server setup
     """
-    global config, today_date, start_date
-    today_date = date.today()
-    start_date = config.get('start_date', today_date)
-    return str((today_date - start_date).days)
-
-
-def get_day_count():
-    global config, today_date, start_date
-    days = getday()
-    return RText(
-        config.get('reply_msg', 'Today, server has been set up for §e{days}§r days!')
-        .format(days=days)
-    ).h(f'§l{start_date}§r -> §l{today_date}§r')
+    return GetDayCount().days
 
 
 def on_load(server: ServerInterface, prev):
-    global config
-    config = get_config(server)
-    server.register_help_message('!!days', 'Get day count since server set up')
+    server.register_help_message('!!day', '显示开服至今的天数')
     server.register_command(
-        Literal('!!days').runs(lambda src: src.reply(get_day_count()))
+        Literal('!!day').runs(lambda src: src.reply(GetDayCount()()))
     )
